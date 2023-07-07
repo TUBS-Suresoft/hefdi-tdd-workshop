06b73675cd6e7c2d18d5487cce2a3931477066bb
Sören Peters <dev.peters@icloud.com>
2023-07-07 12:47:23 +0200

    check temperature

--- hvac/__init__.py
+++ hvac/__init__.py
@@ -11,9 +11,34 @@ class HVAC(Protocol):
     def stop_blowing(self) -> None:
         raise NotImplementedError
 
+    def start_cooling(self) -> None:
+        raise NotImplementedError
+
+    def start_blowing(self) -> None:
+        raise NotImplementedError
+
+    def start_heating(self) -> None:
+        raise NotImplementedError
+
+    def get_temperature(self) -> int:
+        raise NotImplementedError
+
 
 class Controller:
     def __init__(self, hvac: HVAC) -> None:
-        hvac.stop_heating()
-        hvac.stop_cooling()
-        hvac.stop_blowing()
+        self.hvac = hvac
+        self.hvac.stop_heating()
+        self.hvac.stop_cooling()
+        self.hvac.stop_blowing()
+        self.desired_temperature = 0
+        self.temperature_delta = 0
+
+    def check_temperature(self) -> None:
+        self.hvac.start_cooling()
+        self.hvac.start_blowing()
+        self.hvac.stop_heating()
+
+        its_too_cold = self.hvac.get_temperature() < self.desired_temperature + self.temperature_delta
+        if (its_too_cold):
+            self.hvac.stop_cooling()
+            self.hvac.start_heating()
--- test/test_controller.py
+++ test/test_controller.py
@@ -6,6 +6,7 @@ class HVACTestDouble(HVAC):
         self._heating = False
         self._blowing = False
         self._cooling = False
+        self.temperature = 0
 
     def is_heating(self) -> bool:
         return self._heating
@@ -25,11 +26,30 @@ class HVACTestDouble(HVAC):
     def stop_blowing(self) -> None:
         self._blowing = False
 
+    def start_cooling(self) -> None:
+        self._cooling = True
+
+    def start_blowing(self) -> None:
+        self._blowing = True
+
+
+    def start_heating(self) -> None:
+        self._heating = True
+
+    def get_temperature(self) -> int:
+        return self.temperature
+
+
 
 def assert_everything_is_off(hvac: HVACTestDouble) -> None:
     assert hvac.is_heating() == False
     assert hvac.is_blowing() == False
     assert hvac.is_cooling() == False
+
+def assert_is_cooling(hvac: HVACTestDouble) -> None:
+    assert hvac.is_heating() == False
+    assert hvac.is_blowing() == True
+    assert hvac.is_cooling() == True
 
 # Given - When - Then
 def test_when_starting_system__everything_is_off() -> None:
@@ -42,3 +62,33 @@ def test_when_starting_system__everything_is_off() -> None:
 
     # Assert
     assert_everything_is_off(hvac)
+
+
+def test_when_its_too_hot__it_is_start_cooling() -> None:
+    # Arrange
+    hvac = HVACTestDouble()
+    sut = Controller(hvac)
+
+    # Act
+    sut.check_temperature()
+
+    # Assert
+    assert_is_cooling(hvac)
+
+
+def test_when_its_too_cold__it_is_start_heating() -> None:
+    # Arrange
+    hvac = HVACTestDouble()
+    sut = Controller(hvac)
+
+    hvac.temperature = 17
+    sut.desired_temperature = 20
+    sut.temperature_delta = 2
+
+    # Act
+    sut.check_temperature()
+
+    # Assert
+    assert hvac.is_heating() == True
+    assert hvac.is_blowing() == True
+    assert hvac.is_cooling() == False
