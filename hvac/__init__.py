ee70deb3340a59e6abe775b36ddafac0a853b0c4
Sören Peters <dev.peters@icloud.com>
2023-07-07 12:12:59 +0200

    add first passing test

--- hvac/__init__.py
+++ hvac/__init__.py
@@ -1,2 +1,18 @@
+from typing import Protocol
+
+class HVAC(Protocol):
+    def stop_heating(self):
+        raise NotImplementedError
+
+    def stop_cooling(self):
+        raise NotImplementedError
+
+    def stop_blowing(self):
+        raise NotImplementedError
+
+
 class Controller:
-    pass
+    def __init__(self, hvac: HVAC) -> None:
+        hvac.stop_heating()
+        hvac.stop_cooling()
+        hvac.stop_blowing()
--- test/test_controller.py
+++ test/test_controller.py
@@ -1,4 +1,44 @@
 from hvac import Controller
+from hvac import HVAC
 
-def test_controller():
-    assert False
+class HVACTestDouble(HVAC):
+    def __init__(self):
+        self._heating = False
+        self._blowing = False
+        self._cooling = False
+
+    def is_heating(self) -> bool:
+        return self._heating
+
+    def is_cooling(self) -> bool:
+        return self._cooling
+
+    def is_blowing(self) -> bool:
+        return self._blowing
+
+    def stop_heating(self):
+        self._heating = False
+
+    def stop_cooling(self):
+        self._cooling = False
+
+    def stop_blowing(self):
+        self._blowing = False
+
+
+def assert_everything_is_off(hvac: HVACTestDouble):
+    assert hvac.is_heating() == False
+    assert hvac.is_blowing() == False
+    assert hvac.is_cooling() == False
+
+# Given - When - Then
+def test_when_starting_system__everything_is_off():
+
+    # Arrange
+    hvac = HVACTestDouble()
+
+    # Act
+    sut = Controller(hvac)
+
+    # Assert
+    assert_everything_is_off(hvac)
